2d9758339d6c65b811d9a82962fae020da7af734
Igasaki Gouta <igafactory2023@gmail.com>
2025-08-04 23:32:59 +0900

    Initial Commit

---
+++ Matte/MatteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatteApp.swift
+//  Matte
+//
+//  Created by Igasaki Gouta on 2025/08/04.
+//
+
+import SwiftUI
+
+@main
+struct MatteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MatteTests/MatteTests.swift
@@ -0,0 +1,16 @@
+//
+//  MatteTests.swift
+//  MatteTests
+//
+//  Created by Igasaki Gouta on 2025/08/04.
+//
+
+import Testing
+
+struct MatteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
